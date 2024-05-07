2e37c0732411ed7a92f54e04b00f9bfbc0db48b4
artucorreia <arthurcorria0109@gmail.com>
2024-05-06 21:38:54 -0300

    initial commit

---
+++ first-steps/src/main/java/com/artudev/firststeps/Startup.java
@@ -0,0 +1,13 @@
+package com.artudev.firststeps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Startup {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Startup.class, args);
+	}
+
+}
