79d0ab820418861506d21b5f2e4e57ca580c9a56
artucorreia <arthurcorria0109@gmail.com>
2024-05-09 18:19:21 -0300

    update

--- first-project/src/main/java/com/example/firstproject/FirstProjectApplication.java
+++ first-project/src/main/java/com/example/firstproject/FirstProjectApplication.java
@@ -1,13 +1,13 @@
-package com.artudev.firststeps;
+package com.example.firstproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Startup {
+public class FirstProjectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Startup.class, args);
+		SpringApplication.run(FirstProjectApplication.class, args);
 	}
 
 }
--- first-steps/src/test/java/com/example/firststeps/FirstStepsApplicationTests.java
+++ first-steps/src/test/java/com/example/firststeps/FirstStepsApplicationTests.java
@@ -1,10 +1,10 @@
-package com.artudev.firststeps;
+package com.example.firststeps;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class StartupTests {
+class FirstStepsApplicationTests {
 
 	@Test
 	void contextLoads() {
